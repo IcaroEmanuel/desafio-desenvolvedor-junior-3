93ee40836bb0c2ae691896b3118dc702c3e6ad7c
Ícaro Emanuel <34634165+IcaroEmanuel@users.noreply.github.com>
2023-06-18 17:57:19 -0300

    correções no model Post e no arquivo de configuração do sequelize

--- back-end/src/models/post.js
+++ back-end/src/models/post.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -10,15 +8,18 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Post.belongsTo(models.Pessoa)
+      Post.belongsTo(models.User);
     }
   }
-  Post.init({
-    title: DataTypes.STRING,
-    content: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
   return Post;
 };
