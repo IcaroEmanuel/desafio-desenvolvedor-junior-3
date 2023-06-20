93462acb23e76d3ea665b1aa6685113c41b9aade
Ícaro Emanuel <34634165+IcaroEmanuel@users.noreply.github.com>
2023-06-20 14:41:46 -0300

    Resolvendo problemas de acesso a API

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
+
 const routes = require('./routes');
 
 const app = express();
 
 const port = process.env.PORT;
+
+app.use((_request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  response.header('Access-Control-Allow-Headers', '*');
+  app.use(cors());
+  next();
+})
 
 routes(app);
 
