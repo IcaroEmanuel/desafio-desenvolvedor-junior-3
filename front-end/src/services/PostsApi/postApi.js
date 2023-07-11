14613c03133819be7b363dff97c4912066f6aa05
Ícaro Emanuel <34634165+IcaroEmanuel@users.noreply.github.com>
2023-07-11 00:29:35 -0300

    crud completo de posts

--- front-end/src/services/PostsApi/postApi.js
+++ front-end/src/services/PostsApi/postApi.js
@@ -1,17 +1,62 @@
 import { getUserLocalStorage } from "../../context/AuthProvider/util";
 import { Api } from "../api";
 
+const user = getUserLocalStorage();
+const headers = { Authorization: `Bearer ${user.token}` };
+
 const PostsService = {
   getPosts: async () => {
     try {
-      const token = getUserLocalStorage();
-      const headers = { Authorization: `Bearer ${token.token}` };
-      const response = await Api.get("/posts", { headers });
-      return response.data;
+      const post = await Api.get("/posts", { headers });
+      return post.data;
     } catch (error) {
       console.log("error", error);
     }
   },
+
+  getPostById: async (id) => {
+    try {
+      const post = await Api.get(`/posts/${id}`, { headers });
+      return post.data;
+    } catch (error) {
+      console.log("error", error);
+    }
+  },
+
+  createPost: async (userId, title, content) => {
+    const body = {
+      userId,
+      title,
+      content,
+    };
+    try {
+      const post = await Api.post("/posts", body, { headers });
+      return post.data;
+    } catch (error) {
+      console.log("error", error);
+    }
+  },
+
+  editPost: async (id, title, content)  => {
+    const body = {
+      title,
+      content,
+    };
+
+    try {
+      await Api.put(`/posts/${id}`, body, { headers });
+    } catch (error) {
+      console.log("error", error);
+    }
+  },
+
+  deletePost: async (id) => {
+    try {
+      await Api.delete(`posts/${id}`, {headers})
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 
 export default PostsService;
