593d784b938032b54ca32c8d3290f01bde0f23dc
Ícaro Emanuel <34634165+IcaroEmanuel@users.noreply.github.com>
2023-07-11 01:33:54 -0300

    Filtrando posts da pessoa logada

--- front-end/src/pages/Posts/index.jsx
+++ front-end/src/pages/Posts/index.jsx
@@ -16,16 +16,18 @@ export default function Posts() {
   useEffect(() => {
     setUser(getUserLocalStorage());
     const allPosts = async () => {
-      try {
-        const postsData = await PostsService.getPosts();
+      const postsData = await PostsService.getPosts();
 
+      setMyPosts(!myPosts);
+
+      if (myPosts) {
+        setAllPosts(postsData.filter((post) => post.User.email === user.email));
+      } else {
         setAllPosts(postsData);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
       }
     };
     return allPosts;
-  }, []);
+  }, [myPosts, user.email]);
 
   const filterPostsByuser = () => {
     setMyPosts(!myPosts);
@@ -56,7 +58,11 @@ export default function Posts() {
               onClick={filterPostsByuser}
               className="justify-end rounded bg-blue-600 hover:bg-blue-500 p-3 mr-4 mt-2 text-gray-200"
             >
-              Ver apenas meus posts
+              {myPosts ? (
+                <span>Ver apenas meus posts</span>
+              ) : (
+                <span>Ver todos os posts</span>
+              )}
             </button>
             <button
               type="button"
@@ -67,6 +73,12 @@ export default function Posts() {
             </button>
           </div>
         </div>
+        {allPosts.length === 0 && (
+          <p className="text-center text-gray-600">
+            Ainda não existem posts aqui, mas você pode criar o primeiro
+            clicando <a href="/create-post">aqui</a>
+          </p>
+        )}
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {allPosts.map((post) => (
             <article
@@ -88,7 +100,7 @@ export default function Posts() {
                 <div className="relative mt-8 flex items-center gap-x-4">
                   <button
                     type="button"
-                    onClick={(() => navigateEditPost(post.id))}
+                    onClick={() => navigateEditPost(post.id)}
                     className="rounded-full hover:bg-gray-100 p-2 mr-4"
                   >
                     <span className="material-symbols-outlined">edit</span>
